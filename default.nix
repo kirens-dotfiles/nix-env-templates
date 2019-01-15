6825a37f561e8ec19cdda8c1d661d5efa41422c4
Kirens <dev@erik.work>
2019-01-15 11:07:41 +0100

    A simple script to insert environments forom the shells dir

---
+++ default.nix
@@ -0,0 +1,30 @@
+{ stdenv, bash, coreutils, gnugrep }:
+
+stdenv.mkDerivation {
+  name = "mkEnv";
+
+  buildCommand = ''
+    mkdir -p $out/bin $out/shells
+    for filepath in ${./shells}/*.nix
+    do
+      file=$(basename -- "$filepath")
+      cp "$filepath" "$out/shells/''${file%.nix}"
+    done
+
+    install -v -D -m755 ${./mkEnv.sh} $out/bin/mkEnv
+    substituteInPlace $out/bin/mkEnv \
+      --subst-var-by bash "${bash}/bin/bash" \
+      --subst-var-by test "${coreutils}/bin/test" \
+      --subst-var-by echo "${coreutils}/bin/echo" \
+      --subst-var-by ls "${coreutils}/bin/ls" \
+      --subst-var-by cp "${coreutils}/bin/cp" \
+      --subst-var-by grep "${gnugrep}/bin/grep" \
+      --subst-var-by SHELLS "$out/shells"
+  '';
+
+  meta = with stdenv.lib; {
+    description = "Quickly add template shell.nix files";
+    platforms = platforms.unix;
+    license = licenses.unlicense;
+  };
+}
---
+++ mkEnv.sh
@@ -0,0 +1,55 @@
+#! @bash@
+
+file="$1"
+force=f
+
+# Other simple options
+case "$1" in
+  --help)
+    @echo@ 'Usage: mkEnv [OPTION] NAME'
+    @echo@ ''
+    @echo@ 'Quickly add template shell.nix files'
+    @echo@ ''
+    @echo@ 'OPTIONS'
+    @echo@ '  -l, --list  List all available environments'
+    @echo@ '  -f, --force Write shell.nix file even if one already exists'
+    @echo@ ''
+    @echo@ 'NAME'
+    @echo@ '  Name of the environment to inject or querry when listing envs'
+    exit 0
+    ;;
+  --list)
+    if test -z "$2"
+    then
+      @ls@ "@SHELLS@"
+    else
+      @ls@ "@SHELLS@" | @grep@ "$2"
+    fi
+    exit 0
+    ;;
+  -f)
+    ;&
+  --force)
+    file="$2"
+    force=t
+    ;;
+esac
+
+filename="@SHELLS@/$file"
+
+# Test if there is something we can insert first
+if @test@ ! -f "$filename"
+then
+  >2& @echo@ "No default for this environment"
+  exit 127
+fi
+
+# Then make sure we're not overwriting a file unintentionally
+if @test@ -e shell.nix -a $force = f
+then
+  >2& @echo@ "Would be overwriting an existing file use -f to allow this"
+  exit 1
+fi
+
+# Then insert the file
+@cp@ "$filename" shell.nix
